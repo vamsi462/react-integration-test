e6c32527e4e21564de8647e84d57f6aae65a83d0
vamsi462 <vamsikrishnaadususmalli999@gmail.com>
2021-06-09 19:57:24 +0530

    intergration test

---
+++ src/LogingModule/index.integration.test.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { render, fireEvent, waitFor, screen } from "@testing-library/react";
+import { LoginModule } from "./Index";
+
+describe("LoginModule", () => {
+  test("initial state", () => {
+    render(<LoginModule />);
+
+    // it renders empty email and passsword fields
+    const emailField = screen.getByRole("textbox", { name: "Email" });
+    expect(emailField).toHaveValue("");
+    const passwordField = screen.getByLabelText("Password");
+    expect(passwordField).toHaveValue("");
+
+    // it renders enabled submit button
+    const button = screen.getByRole("button");
+    expect(button).not.toBeDisabled();
+    expect(button).toHaveTextContent("Submit");
+  });
+
+  test("successful login", async () => {
+    jest
+      .spyOn(window, "fetch")
+      .mockResolvedValue({ json: () => ({ token: "123" }) });
+
+    render(<LoginModule />);
+
+    const emailField = screen.getByRole("textbox", { name: "Email" });
+    const passwordField = screen.getByLabelText("Password");
+    const button = screen.getByRole("button");
+
+    // fill out and submit form
+    fireEvent.change(emailField, { target: { value: "test@email.com" } });
+    fireEvent.change(passwordField, { target: { value: "password" } });
+    fireEvent.click(button);
+
+    // it sets loading state
+    expect(button).toBeDisabled();
+    expect(button).toHaveTextContent("Loading");
+
+    await waitFor(() => {
+      // it hides form elements
+      expect(button).not.toBeInTheDocument();
+      expect(emailField).not.toBeInTheDocument();
+      expect(passwordField).not.toBeInTheDocument();
+
+      // it displays success text and email address
+      const loggedInText = screen.getByText("Logged in as");
+      expect(loggedInText).toBeInTheDocument();
+      const emailAddressText = screen.getByText("test@email.com");
+      expect(emailAddressText).toBeInTheDocument();
+    });
+  });
+});
